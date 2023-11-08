78318e1addfd2670665bf5089c58a166b5d8f171
yourLogic01 <2010631170004@student.unsika.ac.id>
2023-11-08 19:01:29 +0700

    fix: fixing bug on dashboard while delete post

--- database/migrations/2023_10_21_162131_create_comments_table.php
+++ database/migrations/2023_10_21_162131_create_comments_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
